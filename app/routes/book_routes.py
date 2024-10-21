beb57d681b1334154c2101c4af51f65439d1e0da
Beenish <beenishali693@gmail.com>
2024-10-21 09:30:05 -0700

    02b-404

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
@@ -18,12 +18,26 @@ def get_all_books():
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
+            book = validate_book(book_id)
+    
             return {
                 "id": book.id,
                 "title": book.title, 
                 "description": book.description
             }
         
+        
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+    
+    for book in books:
+        if book.id == book_id:
+            return book
+    
+    response = {"message": f"book {book_id} not found"}, 404
+    abort(make_response(response, 404))
+
