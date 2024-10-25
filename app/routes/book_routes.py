41f074a473bd4e3582adc16e396baadace072226
Beenish <beenishali693@gmail.com>
2024-10-25 10:16:03 -0500

    03b-create

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,19 +1,26 @@
-from flask import Blueprint, abort, make_response
-# from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return response, 201
+
 #     return books_response
 
 # @books_bp.get("/<book_id>")
