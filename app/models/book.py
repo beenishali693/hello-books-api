e6e86cc59d9c55f9e9d1aa5993c8e8508879ae1c
Beenish <beenishali693@gmail.com>
2024-11-05 09:02:41 -0800

    07c-validate-book-refactor

--- app/models/book.py
+++ app/models/book.py
@@ -1,8 +1,6 @@
 # class Book:
 #     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
+#         
 
 # books = [
 #     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
@@ -17,3 +15,17 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+
+    def to_dict(self):
+        return dict(
+        id=self.id,
+        title=self.title,
+        description=self.description
+        )
+
+    @classmethod
+    def from_dict(cls, book_data):
+        return cls(
+            title=book_data["title"],
+            description=book_data["description"]
+        )
