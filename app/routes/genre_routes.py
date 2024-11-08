9d0ef469851d73a4b112f02fefc2e519b588da30
Beenish <beenishali693@gmail.com>
2024-11-08 12:50:05 -0800

    10c-nested-routes

--- app/routes/genre_routes.py
+++ app/routes/genre_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.genre import Genre
+from app.models.book import Book
 from ..db import db
 from .route_utilities import validate_model, create_model, get_models_with_filters
 
@@ -13,3 +14,17 @@ def create_genre():
 @bp.get("")
 def get_all_genres():
     return get_models_with_filters(Genre, request.args)
+
+@bp.post("/<genre_id>/books")
+def create_book_with_genre(genre_id):
+    genre = validate_model(Genre, genre_id)
+
+    request_body = request.get_json()
+    request_body["genres"] = [genre]
+    return create_model(Book, request_body)
+
+@bp.get("/<genre_id>/books")
+def get_books_by_genre(genre_id):
+    genre = validate_model(Genre, genre_id)
+    response = [book.to_dict() for book in genre.books]
+    return response
