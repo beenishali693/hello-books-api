03a14d3c1b50809f65f8218d7d1ebe852c3ead32
Beenish <beenishali693@gmail.com>
2024-11-08 12:32:44 -0800

    10b-book-genres

--- app/models/book.py
+++ app/models/book.py
@@ -15,6 +15,7 @@ from ..db import db
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .author import Author
+    from .genre import Genre
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -22,7 +23,8 @@ class Book(db.Model):
     description: Mapped[str]
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
-
+    genres: Mapped[list["Genre"]] = relationship(secondary="book_genre", back_populates="books")
+    
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
